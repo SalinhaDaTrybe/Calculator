3574a8d1de31ef568d1bfe2ba9689f6a659c6346
wanderson-vicente <wandersonvicente@gmail.com>
2023-07-28 15:25:10 -0300

    rascunho logico 1

--- script.js
+++ script.js
@@ -7,28 +7,39 @@ const opPlus = document.getElementById("plus");
 const opMinus = document.getElementById("minus");
 const opMultiply = document.getElementById("multiply");
 const opDivide = document.getElementById("divide");
-const opClear = document.getElementById("clear");;
+const result = document.getElementById("divide");
+const opClear = document.getElementById("clear");
+
+
 
 ///////////////////////////////////////////////////////////////////
 //      função da operação
 ////////////////////////////////////////////////////////////////////
-const operacao = (n1, n2, operador) => {
+const operacao = (operador) => {
+  let n1 = 10;
+  let n2 = 8;
+  // let operador = 
   switch (operador) {
     case '+':
-      return n1+n2;
-    case '-':
+      return console.log(n1+n2);
+      case '-':
       return n1-n2;
     case '*':
       return n1*n2;
     case '/':
       return n1/n2;
-    case '/':
+    case 'c':
       // limpar number1,number2 e resposta
     default:
       break;
-  }
-};
-
-///////////////////////////////////////////////////////////////////
-//      ouvindo o evento click
-////////////////////////////////////////////////////////////////////
+    }
+  };
+  
+  ///////////////////////////////////////////////////////////////////
+  //      ouvindo o evento click
+  ////////////////////////////////////////////////////////////////////
+  const soma = (opPlus.addEventListener('click', operacao('+')));
+  const subtrai = (opPlus.addEventListener('click', operacao('-')));
+  const multiplica = (opPlus.addEventListener('click', operacao('*')));
+  const divide = (opPlus.addEventListener('click', operacao('/')));
+  const solimpama = (opPlus.addEventListener('click', operacao('c')));
