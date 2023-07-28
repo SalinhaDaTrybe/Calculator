ba18e05b43e82f502c898086312061d7ff2e580d
wanderson-vicente <wandersonvicente@gmail.com>
2023-07-28 14:50:03 -0300

    Captura e esboço da função de operação

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+///////////////////////////////////////////////////////////////////
+//      Captura
+////////////////////////////////////////////////////////////////////
 const op1 = document.getElementById("number1");
 const op2 = document.getElementById("number2");
 const opPlus = document.getElementById("plus");
@@ -5,3 +8,27 @@ const opMinus = document.getElementById("minus");
 const opMultiply = document.getElementById("multiply");
 const opDivide = document.getElementById("divide");
 const opClear = document.getElementById("clear");;
+
+///////////////////////////////////////////////////////////////////
+//      função da operação
+////////////////////////////////////////////////////////////////////
+const operacao = (n1, n2, operador) => {
+  switch (operador) {
+    case '+':
+      return n1+n2;
+    case '-':
+      return n1-n2;
+    case '*':
+      return n1*n2;
+    case '/':
+      return n1/n2;
+    case '/':
+      // limpar number1,number2 e resposta
+    default:
+      break;
+  }
+};
+
+///////////////////////////////////////////////////////////////////
+//      ouvindo o evento click
+////////////////////////////////////////////////////////////////////
