69143e39098c53b5127b34b537c6cbf85bb127ee
wanderson-vicente <wandersonvicente@gmail.com>
2023-07-28 14:33:58 -0300

    Botões arredondados, adição do botão clear e captura

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//começar
+const op1 = document.getElementById("number1");
+const op2 = document.getElementById("number2");
+const opPlus = document.getElementById("plus");
+const opMinus = document.getElementById("minus");
+const opMultiply = document.getElementById("multiply");
+const opDivide = document.getElementById("divide");
+const opClear = document.getElementById("clear");;
